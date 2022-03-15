dccd1a5c461353f4d5a3f024caac4a3dda3dfa9e
Utsav Kumar <45901934+Utsav418@users.noreply.github.com>
2022-03-15 10:13:25 +0530

    textutil hosted

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import React, { useState } from 'react'
 import Alert from './components/Alert';
-// import About from './components/About';
-// import {
-//   BrowserRouter as Router,
-//   Switch,
-//   Routes,
-//   Route,
-//   Link
-// } from 'react-router-dom';
+import About from './components/About';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Routes,
+  Route,
+  Link
+} from 'react-router-dom';
 
 function App() {
   const [mode, setMode] = useState('light');
@@ -43,19 +43,19 @@ function App() {
   }
   return (
     <>
-      {/* <Router> */}
-      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
-      <Alert alert={alert} />
-      <div className="container">
-        {/* <Routes> */}
-        {/* <Route exact path="/about" element={<About />}>
-            </Route> */}
-        {/* <Route exact path="/" element={ */}
-        <TextForm heading="Enter text to analyze" mode={mode} showAlert={showAlert} />
-        {/* </Route>
-          </Routes> */}
-      </div>
-      {/* </Router> */}
+      <Router>
+        <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
+        <div className="container">
+          <Routes>
+            <Route exact path="/about" element={<About />}></Route>
+            <Route exact path="/" element={
+              <TextForm heading="Enter text to analyze" mode={mode} showAlert={showAlert} />
+            }>
+              </Route>
+          </Routes>
+        </div>
+      </Router> 
     </>
   );
 }
